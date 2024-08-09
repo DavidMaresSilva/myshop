2a3f2c6ca747da396e4c44c63c931d5252cb0cff
David Mares Silva <cienciadedatos.dateadores@gmail.com>
2024-08-09 12:37:34 -0600

    3 registration and wtform

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, session, request, redirect, url_for
+from flask import render_template, session, request, redirect, url_for, flash
 
 from shop import app, db
+from .forms import RegistrationForm
 
 @app.route("/")
 @app.route("/home")
@@ -8,6 +9,14 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/register")
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('/admin/register.html', title='Register user')
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form, title='Registro')
